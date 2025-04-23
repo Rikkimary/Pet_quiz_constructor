c0bfc24d89c6a94b13f7dc9cc591963c84206420
gorbylevai <gorbylev2009@gmail.com>
2025-04-23 15:58:58 +0300

    5 коммит. Обновил  Сеттингс добавил открытие через env

--- Backend/drf_quiz/drf_quiz/settings.py
+++ Backend/drf_quiz/drf_quiz/settings.py
@@ -19,6 +19,7 @@ load_dotenv()
 
 
 DATABASES= {
+
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'pet_quiz_constructor',
@@ -31,8 +32,9 @@ DATABASES= {
         }
     },
 }
-# db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
-# DATABASES['default'].update(db_from_env)
+
+db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'].update(db_from_env)
 # # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # BASE_DIR = "http://127.0.0.1:8000/"
